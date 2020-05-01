8171517a861854dbe8ad3db12dee419229a67335
Piyush <piyush.mittal004@gmail.com>
2020-05-01 20:12:53 +0530

    2nd commit

---
+++ src/app/enter-lab-data/enter-lab-data.component.ts
@@ -0,0 +1,65 @@
+import { Component, OnInit } from '@angular/core';
+import { Mycoplasma } from '../mycoplasma';
+import { RackServiceService } from '../rack-service.service';
+import { LabLinking } from '../lab-linking';
+const ELEMENT_DATA:Mycoplasma[]=
+[
+   {sampleNo:123,result:"Positive",status:true,date: new Date(),name: "Rahul"}
+]
+let sample_list=[];
+@Component({
+  selector: 'app-enter-lab-data',
+  templateUrl: './enter-lab-data.component.html',
+  styleUrls: ['./enter-lab-data.component.css']
+})
+
+
+export class EnterLabDataComponent implements OnInit {
+
+  constructor(public  myco:Mycoplasma,public service:RackServiceService) {
+    this.todayDate= new Date();
+    console.log(this.todayDate);
+   }
+  todayDate;
+  list_sample=[];
+  selectResult;
+  selectStatus;
+  result=["Positive","Negative"];
+  status=["Complete","Incomplete"];
+  selectLab: string;
+  selectSample;
+  samples: number[] =[1,2,3,4];
+  labs: string[] = ['Mycloplasma', 'Geneprint', 'Kyrotyping', 'PBMC','DNA'];
+  ngOnInit() {
+     this.service.getSampleList().subscribe(response => this.fetchData(response));
+    
+  }
+
+
+   fetchData(response:LabLinking[])
+   {
+     console.log(response);
+     let i=0;
+      for(i=0;i<response.length;i++)
+      {
+        sample_list.push(response[i].id.sampleNo);
+      }
+      console.log(`Sample list is : ${this.list_sample}`);
+      this.list_sample=sample_list;
+   }
+  onSaveMyco()
+  {
+    this.myco.sampleNo=this.selectSample;
+    this.myco.date=this.todayDate;
+    this.myco.result=this.selectResult;
+    if(this.selectStatus==this.status[0])
+    this.myco.status=true;
+    else
+    this.myco.status=false;
+    console.log(`Mycoplasma is ${this.myco}`);
+    this.service.updateMycoplasma(this.myco).subscribe();
+  
+    
+  }
+
+}
---
+++ src/app/lab-linking.ts
@@ -0,0 +1,11 @@
+import { id } from './id';
+
+export class LabLinking {
+    id:id;
+    dnaStatus:number;
+    geneprintStatus:number;
+    kyrotypingStatus:number;
+    lCLStatus:number;
+    mycoplasmaStatus:number;
+    pbmncStatus:number;
+}
