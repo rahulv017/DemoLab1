febf9b7ad6f36fbfcb2566c2727088ddd2ceebdf
Piyush <piyush.mittal004@gmail.com>
2020-05-18 12:54:00 +0530

    login updated

--- src/app/input-service.service.ts
+++ src/app/input-service.service.ts
@@ -20,15 +20,15 @@ export class InputServiceService {
   constructor(private route:Router, private _http:HttpClient) { }
 
    private _url:string="https://localhost:8443/search";
-   private jwtToken:string="Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJQaXl1c2giLCJleHAiOjE1ODY5NzUzMDgsImlhdCI6MTU4NjkzOTMwOH0.eltQ1vAmfLjmzarkarnP466hCtteqvN6oix49qx6-OY";
+ //  private jwtToken:string="Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJQaXl1c2giLCJleHAiOjE1ODY5NzUzMDgsImlhdCI6MTU4NjkzOTMwOH0.eltQ1vAmfLjmzarkarnP466hCtteqvN6oix49qx6-OY";
 
   getUserData(patientID:number):Observable<User[]>
   {
    let JsonData={'adbsID':[patientID]};
-     const headers = { 'Authorization': this.jwtToken};
+   //  const headers = { 'Authorization': this.jwtToken};
      console.log(patientID);
      // return this._http.get<User[]>(this._url);
-     return this._http.post<User[]>(this._url,JsonData,{headers});
+     return this._http.post<User[]>(this._url,JsonData);
     
   }
 
@@ -36,9 +36,9 @@ export class InputServiceService {
   {
    //let JsonData={'adbsID':[patientID]};
    let _url1 : string="https://localhost:8443/brieftable"
-     const headers = { 'Authorization': this.jwtToken};
+  //   const headers = { 'Authorization': this.jwtToken};
    //  console.log(patientID);
-      return this._http.get<User[]>(_url1,{headers});
+      return this._http.get<User[]>(_url1);
     // return this._http.post<User[]>(this._url,JsonData,{headers});
     
   }
@@ -47,20 +47,20 @@ export class InputServiceService {
   {
     let _url1:string="https://localhost:8443/filter";
   //  let JsonData={'adbsID':[patientID]};
-     const headers = { 'Authorization': this.jwtToken};
+   //  const headers = { 'Authorization': this.jwtToken};
      console.log(data);
      // return this._http.get<User[]>(this._url);
-     return this._http.post<User[]>(_url1,data,{headers});
+     return this._http.post<User[]>(_url1,data);
   }
 
   getLabStatusDetails():Observable<Status[]>
   {
     let _url1:string="https://localhost:8443/getstatus";
     //let JsonData={'assess_id':data.Assessment_ID};
-    const headers = { 'Authorization': this.jwtToken};
+  //  const headers = { 'Authorization': this.jwtToken};
    // console.log(JsonData);
     // return this._http.post<Status>(_url1,JsonData,{headers});
-     return this._http.get<Status[]>(_url1,{headers}).pipe(catchError(this.handleError));
+     return this._http.get<Status[]>(_url1,).pipe(catchError(this.handleError));
      
 
   }
@@ -85,9 +85,9 @@ export class InputServiceService {
   {
     let _url1:string="https://localhost:8443/getlabdetails";
     //let JsonData={'assess_id':data.Assessment_ID};
-    const headers = { 'Authorization': this.jwtToken};
+  //  const headers = { 'Authorization': this.jwtToken};
     console.log(data);
-     return this._http.post<Status>(_url1,data,{headers});
+     return this._http.post<Status>(_url1,data);
      //return this._http.get<LCL[]>(_url1);
   }
 
@@ -95,50 +95,50 @@ export class InputServiceService {
   {
     let _url1:string="https://localhost:8443/updatebrieftable";
    // let JsonData={'assess_id':data.Assessment_ID};
-    const headers = { 'Authorization': this.jwtToken};
+ //   const headers = { 'Authorization': this.jwtToken};
     console.log(data);
     // return this._http.post<Status>(_url1,JsonData,{headers});
-     return this._http.put(_url1,data,{headers});
+     return this._http.put(_url1,data);
   }
 
   getCompleteDeepTable():Observable<Deep[]>
   {
     let _url1:string="https://localhost:8443/deeptable";
    // let JsonData={'assess_id':data.Assessment_ID};
-    const headers = { 'Authorization': this.jwtToken};
+   // const headers = { 'Authorization': this.jwtToken};
    // console.log(data);
     // return this._http.post<Status>(_url1,JsonData,{headers});
-     return this._http.get<Deep[]>(_url1,{headers});
+     return this._http.get<Deep[]>(_url1);
   }
 
   getUserDataByAssID(patientID:string):Observable<User>
   {
     let _url1:string="https://localhost:8443/searchbyassessment"
    let JsonData={"D_no":patientID};
-     const headers = { 'Authorization': this.jwtToken};
+  //   const headers = { 'Authorization': this.jwtToken};
      console.log(patientID);
       //return this._http.get<User>(this._url);
-     return this._http.post<User>(_url1,JsonData,{headers});
+     return this._http.post<User>(_url1,JsonData);
     
   }
   saveDeepUser(data:Deep)
   {
     let _url1:string="https://localhost:8443/insert";
    // let JsonData={'assess_id':data.Assessment_ID};
-    const headers = { 'Authorization': this.jwtToken};
+  //  const headers = { 'Authorization': this.jwtToken};
     console.log(data);
     // return this._http.post<Status>(_url1,JsonData,{headers});
-     return this._http.post(_url1,data,{headers});
+     return this._http.post(_url1,data);
   }
 
   getDashboardData():Observable<Dashboard[]>
   {
     let _url1:string="https://localhost:8443/dashboard";
     // let JsonData={'assess_id':data.Assessment_ID};
-     const headers = { 'Authorization': this.jwtToken};
+  //   const headers = { 'Authorization': this.jwtToken};
     // console.log(data);
      // return this._http.post<Status>(_url1,JsonData,{headers});
-      return this._http.get<Dashboard[]>(_url1,{headers});
+      return this._http.get<Dashboard[]>(_url1);
   }
 
 }
