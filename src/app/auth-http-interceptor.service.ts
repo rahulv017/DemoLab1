9c3da368b93059cfc8b55d9d8005471e9fff0731
Rahul <vyasrahul017@gmail.com>
2020-05-19 12:33:25 +0530

    Handled JWT Expire Exception

--- src/app/auth-http-interceptor.service.ts
+++ src/app/auth-http-interceptor.service.ts
@@ -1,12 +1,15 @@
 import { Injectable } from '@angular/core';
-import { HttpInterceptor, HttpRequest, HttpHandler } from '@angular/common/http';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpErrorResponse } from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthHttpInterceptorService implements HttpInterceptor {
   
-  constructor() { }
+  constructor(private router:Router) { }
   
   intercept(req: HttpRequest<any>,next:HttpHandler){
     if (sessionStorage.getItem('username') && sessionStorage.getItem('token')) {
@@ -16,6 +19,29 @@ export class AuthHttpInterceptorService implements HttpInterceptor {
         }
       })
     }
-    return next.handle(req);
+    return next.handle(req).pipe(catchError(this.handleError))
   }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      sessionStorage.clear()
+      this.router.navigate(['']);
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error.message);
+    } else {
+
+      sessionStorage.clear()
+      this.router.navigate(['']);
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      console.error(
+        `Backend returned code ${error.status}, ` +
+        `body was: ${error.error}`);
+    }
+    // return an observable with a user-facing error message
+    return throwError(
+      'Something bad happened; please try again later.');
+     
+
+  };
 }
