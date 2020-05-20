eeeae5022b8060e6aaefae66d7e518c1acbbe174
Piyush <piyush.mittal004@gmail.com>
2020-05-20 20:46:08 +0530

    multiple filtering fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule }   from '@angular/forms';
+import { FormsModule,ReactiveFormsModule }   from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {MatSelectModule} from '@angular/material/select';
@@ -105,6 +105,7 @@ import { BackgroundCohortComponent } from './background-cohort/background-cohort
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    ReactiveFormsModule,
     MatCheckboxModule,
     MatSelectModule,
     MatRadioModule,
